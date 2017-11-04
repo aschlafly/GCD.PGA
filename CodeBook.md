82a9e3670d0fa1e340b8d541e2925cd6fd882fbc
Aaron Schlafly <aschlafly@hotmail.com>
2017-11-04 23:45:48 +0900

    Second commit to add the rest of the files

---
+++ CodeBook.md
@@ -0,0 +1,48 @@
+---
+title: "CodeBook.md"
+author: "Aaron Schlafly"
+date: "10/28/2017"
+output: html_document
+---
+
+## Data codebook
+
+Apologies to the user, but the varaible names represent complex concepts, and don't lend themselves to easy summarization as column labels. This codebook is intended to help understand what the readings represent.  
+
+** subject ** contains the number to identify the subject being measured. Users are anonymous, so simply labled with integers.  
+1-30 are the allowable indicators for the 30 subjects.  
+
+** activity_code ** (character lables) correspond to the activity the subject was doing when measurements were taken. Possible values:  
+1. Walking  
+2. Walking upstairs  
+3. Walking downstairs  
+4. Sitting  
+5. Standing  
+6. Laying  
+
+** mean and standard deviation of measurements **
+This is best described by splitting the labels into constiuent pieces.
+
+** first word (character) ** represents the time domain signal itself or the frequency domain signal.  
+*"t" corresponds to time domain signal  
+*"f" corresponds to frequency domain signal  
+
+** second word ** determines whether what is being measured is the body's movement or, in the case of acceleration, whether it is the effect of gravity.  
+*"Body" refers to a measurement of the body's movement  
+*"Gravity" refers to the effect of gravity, in the case of acceleration  
+
+** third word ** determines whether the measurement is from the phone's accelerometer or gyroscope. There are also Jerk accelerations and gyroscope readings.  
+*"Acc" means that the accelerometer took the reading.  
+*"AccJerk" means the Jerk reading corresponding to the accelerometer reading.  
+*"Gyro" means that the gyroscope took the reading.  
+*"GyroJerk" means the Jerk reading corresponding to the gyroscope reading.  
+
+** fourth word ** determines whether the mean or the standard deviation of the readings is in the column.  
+*"mean" means mean is being measured.  
+*"std" means standard deviation is being measured.  
+
+** X, Y, or Z ** are used in the case of three-dimesional measurements.  
+*"X" is used to measure the X-direction component of the measurement.  
+*"Y" is used to measure the Y-direction component of the measurement.  
+*"Z" is used to measure the Z-direction component of the measurement.  
+
---
+++ run_analysis.R
@@ -0,0 +1,67 @@
+## Step 1: Merge the training and test data sets to create one data set
+
+## Set working directory to be the home of the UCI HAR Dataset
+setwd("~/Other/Coursera/GCData/PGA/UCI HAR Dataset")
+
+## Read in the label names and features.
+activity_labels <- read.table("activity_labels.txt")
+features <- read.table("features.txt")
+
+## For the test and training data, read in the subject file, and X and Y files.
+setwd("~/Other/Coursera/GCData/PGA/UCI HAR Dataset/test")
+subject_test <- read.table("subject_test.txt", col.names = "subject")
+X_test <- read.table("X_test.txt", col.names = features[,2])
+Y_test <- read.table("Y_test.txt", col.names = "activity_code")
+
+setwd("~/Other/Coursera/GCData/PGA/UCI HAR Dataset/train")
+subject_train <- read.table("subject_train.txt", col.names = "subject")
+X_train <- read.table("X_train.txt", col.names = features[,2])
+Y_train <- read.table("Y_train.txt", col.names = "activity_code")
+
+## Append the training data to the end of the test data
+subject_all <- rbind(subject_test, subject_train)
+X_all <- rbind(X_test,X_train)
+Y_all <- rbind(Y_test,Y_train)
+
+## Step 3: (I will do step 3 before step 2, since it is easier to layer in 
+## with labels already attached)
+## 
+
+library(plyr)
+## Add a column to Y_all which maps the codes to descriptive labels
+Y_all <- mutate(Y_all, activity_labels = activity_labels[Y_all[,1],2])
+
+
+
+## Back to step 1:
+## Now combine the subject codes, activities (with descriptors), and all X data
+XY_all <- cbind(subject_all, Y_all, X_all)
+
+
+## Step 2: Extract only the measurements on the mean and standard deviation
+## for each measurement
+## I interpret "mean" and "standard deviation" to only be those variables which
+## have mean() or std() in the name, in which I search for "mean(" or "std("
+
+## First, select only those indices which have "mean(" or "std("
+mean_sd_indices <- grep("mean\\(|std\\(",features$V2)
+
+## Select the first three columns along with the columns with "mean(" or "std("
+indices <- c(c(1:3),mean_sd_indices + 3)
+
+## Then select only those columns corresponding to "mean(" or "std("
+XY <- XY_all[,indices]
+
+## Use plyr fucntions to split by subject and descriptive activity
+## then calculate the mean, removing NAs
+
+library(dplyr)
+tidydt <- XY %>% 
+    group_by(subject, activity_labels) %>% 
+    summarize_at(vars(tBodyAcc.mean...X:fBodyBodyGyroJerkMag.std..),
+                 mean, na.rm = TRUE) 
+
+## Write the file to the correct directory
+setwd("~/Other/Coursera/GCData/PGA")
+write.table(tidydt,file = "tidydt.csv")
+
